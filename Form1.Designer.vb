175407e7c3117526b962498d76e7fc1ec61ffecf
Dieoscar <dkloster79@yahoo.com.ar>
2024-06-17 19:46:18 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        cmdEjecutar = New Button()
+        Label1 = New Label()
+        txtNumeroN = New TextBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' cmdEjecutar
+        ' 
+        cmdEjecutar.BackColor = Color.CornflowerBlue
+        cmdEjecutar.BackgroundImageLayout = ImageLayout.Stretch
+        cmdEjecutar.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        cmdEjecutar.ForeColor = SystemColors.ControlText
+        cmdEjecutar.ImageAlign = ContentAlignment.MiddleLeft
+        cmdEjecutar.Location = New Point(726, 486)
+        cmdEjecutar.Name = "cmdEjecutar"
+        cmdEjecutar.Size = New Size(183, 62)
+        cmdEjecutar.TabIndex = 0
+        cmdEjecutar.Text = "Ejecutar"
+        cmdEjecutar.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Verdana", 12F)
+        Label1.Location = New Point(22, 68)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(699, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Cargar un Numero para determinar si existen Multiplos en el Vector"
+        ' 
+        ' txtNumeroN
+        ' 
+        txtNumeroN.Font = New Font("Verdana", 15F)
+        txtNumeroN.Location = New Point(305, 108)
+        txtNumeroN.Name = "txtNumeroN"
+        txtNumeroN.Size = New Size(110, 38)
+        txtNumeroN.TabIndex = 2
+        txtNumeroN.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.WhiteSmoke
+        Label2.Font = New Font("Verdana", 12F)
+        Label2.Location = New Point(35, 176)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(76, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "Vector"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(921, 560)
+        Controls.Add(Label2)
+        Controls.Add(txtNumeroN)
+        Controls.Add(Label1)
+        Controls.Add(cmdEjecutar)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents cmdEjecutar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNumeroN As TextBox
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Dim contador As Integer
+        Dim i As Integer
+        Dim impares As Integer
+        Dim m As Integer
+        Dim mayor As Integer
+        Dim multiplo As Integer
+        Dim n As Integer
+        Dim numeron As Integer
+        Dim p As Integer
+        Dim pares As Integer
+        Dim posicion As Integer
+        Dim resto As Double
+        Dim x As Integer
+        Dim vector(10) As Integer
+        Dim par(10) As Integer
+        Dim impar(10) As Integer
+        Dim vectorinverso(10) As Integer
+        Dim multiplos(10) As Integer
+        pares = 1
+        impares = 1
+        multiplo = 1
+        For i = 1 To 10
+            vector(i) = InputBox()
+        Next i
+        Console.WriteLine("Ingrese un N° para determinar si existen múltiplos en el Vector")
+        numeron = Integer.Parse(Console.ReadLine())
+        For n = 1 To 10
+            resto = vector(n) Mod 2
+            If resto = 0 Then
+                par(pares) = vector(n)
+                pares = pares + 1
+            Else
+                impar(impares) = vector(n)
+                impares = impares + 1
+            End If
+        Next n
+        Console.Write("N° Par: ")
+        For p = 1 To 10
+            If par(p) <> 0 Then
+                Console.Write(" - ", par(p))
+            End If
+        Next p
+        Console.WriteLine(" ")
+        Console.Write("N° Impar: ")
+        For i = 1 To 10
+            If impar(i) <> 0 Then
+                Console.Write(" - ", impar(i))
+            End If
+        Next i
+        For m = 1 To 10
+            If vector(m) > mayor Then
+                mayor = vector(m)
+                posicion = m
+            End If
+        Next m
+        Console.WriteLine(" ")
+        Console.WriteLine("El N° Mayor es: ", mayor, " fue ingresado en la: ", posicion, " Posicion")
+        For m = 1 To 10
+            resto = vector(m) Mod numeron
+            If resto = 0 Then
+                multiplos(multiplo) = vector(m)
+                multiplo = multiplo + 1
+            End If
+        Next m
+        Console.Write("Los Multiplos de N son: ")
+        For m = 1 To 10
+            If multiplos(m) <> 0 Then
+                Console.Write(" - ", multiplos(m))
+            End If
+        Next m
+        Console.WriteLine(" ")
+        contador = 10
+        For x = 1 To 10
+            vectorinverso(x) = vector(contador)
+            contador = contador - 1
+        Next x
+        Console.WriteLine("Vector Inverso")
+        For i = 1 To 10
+            Console.WriteLine(vectorinverso(i))
+        Next i
+    End Sub
+
+
+End Class
